fc7f3afbf8ef01b627a610c37e540178c49e3602
poqopo <95621149+poqopo@users.noreply.github.com>
2025-11-25 21:39:29 +0900

    fix/Header 오류 조정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,9 +8,9 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="bg-orange-50 absolutely top-0 z-100 shadow-sm">
-        <div className="container mx-auto px-8 max-w-7xl">
-          <div className="flex justify-center items-center h-12 my-1">
+      <header className="bg-orange-50 absolute top-0 z-100 shadow-sm w-full py-2 border-b border-gray-200">
+        <div className="container mx-auto px-8 w-full">
+          <div className="flex justify-center items-center md:justify-start h-12">
             <Link
               to="/"
               className="text-2xl font-bold text-center text-gray-900 hover:text-orange-600 transition-colors"
@@ -20,7 +20,7 @@ export default function Layout({ children }: LayoutProps) {
           </div>
         </div>
       </header>
-      <main className="flex-1 w-full">{children}</main>
+      <main className="flex-1 w-full pt-16 md:pt-0">{children}</main>
       <footer className="bg-foreground text-background mt-auto">
         <div className="container mx-auto px-8 max-w-7xl py-8">
           <p className="text-center">
